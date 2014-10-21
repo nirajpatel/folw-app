0a983723b117d334705164efba977d2298413e42
Brandon Lee <Kyouma@wireless-10-147-111-209.public.utexas.edu>
2014-10-20 20:42:31 -0500

    added initial mapviewcontroller

---
+++ folw/folw/FolwMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  FolwMapViewController.h
+//  folw
+//
+//  Created by Brandon Lee on 10/20/14.
+//  Copyright (c) 2014 CS378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FolwMapViewController : UIViewController
+
+@end
